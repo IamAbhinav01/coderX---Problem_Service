d601a81503d6c4dc0f9d5f886a7c1bededf52edd
Abhinav <AbhinavSunil@hotmail.com>
2026-07-17 10:42:59 +0530

    Feat : Implemented Problem Schema for build pROBELM repository , moving forward with crud operation of problems

--- DB/repositories/problems.repositories.go
+++ DB/repositories/problems.repositories.go
@@ -1,1 +1,34 @@
 package repositories
+
+import (
+	"coderX/models"
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ProblemRepository interface {
+	CreateProblem() (*mongo.InsertOneResult, error)
+	GetProblem()
+	GetAllProblems()
+	UpdateProbelm()
+	DeleteProblem()
+}
+
+type ProblemRepositoryImpl struct {
+	db *mongo.Collection
+}
+
+func(repo *ProblemRepositoryImpl) CreateProblem() (*mongo.InsertOneResult, error){
+
+	var problem models.Problem
+	result,err := repo.db.InsertOne(context.Background(),problem)
+	if err != nil{
+		log.Println("Error occured while storing the problem data to MongoDB")
+		return nil,err
+	}
+	
+	return result,nil
+
+}

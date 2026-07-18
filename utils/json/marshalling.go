3e1940c7e3b212c12eaa9c71dc5bba491495c43f
Abhinav <AbhinavSunil@hotmail.com>
2026-07-18 15:02:49 +0530

    Feat : Implemented JSON CONVERTER, now moving forward to setup the the middleware for validation

---
+++ utils/json/marshalling.go
@@ -0,0 +1,14 @@
+package json
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func ConvertTOJSON(w http.ResponseWriter,status int,data any)error{
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(data)
+}

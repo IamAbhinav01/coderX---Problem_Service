f86e4eef82d5198695c0b3e458b42017a935f8cf
Abhinav <AbhinavSunil@hotmail.com>
2026-07-18 14:53:35 +0530

    Feat : Working on controller layer and now requires marshamalling

---
+++ controllers/problem.controller.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"coderX/services"
+	"net/http"
+)
+
+type ProblemController struct {
+	service services.ProblemService
+}
+
+func NewProblemController (_service services.ProblemService) *ProblemController{
+	return &ProblemController{
+		service: _service,
+	}
+}
+
+func (controller *ProblemController) CreateProblem(w http.ResponseWriter,r *http.Request){
+	controller.service.CreateProblem(r.Context(),)
+}
+
+
+
+func (c *ProblemController) CreateProblemHandler(w http.ResponseWriter, r *http.Request) {
+	var problemPayload models.Problem
+
+	// 1. Parse JSON body
+	if err := json.NewDecoder(r.Body).Decode(&problemPayload); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// 2. Call service layer
+	createdProblem, err := c.service.CreateProblem(r.Context(), &problemPayload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// 3. Return JSON response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"Success": true,
+		"Message": "Problem created successfully",
+		"data":    createdProblem,
+	})
+}

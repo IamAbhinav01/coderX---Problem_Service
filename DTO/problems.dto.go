3b2a1f65de5872e168d5a2eec2a144ab715e3cc4
Abhinav <AbhinavSunil@hotmail.com>
2026-07-19 23:35:55 +0530

    Feat : Implemented the Crud for create with proper validation

--- DTO/problems.dto.go
+++ DTO/problems.dto.go
@@ -8,17 +8,17 @@ type TestCase struct {
 type CodeSnippet struct {
 	Language     string `bson:"language" json:"language" validate:"required"`
 	StartSnippet string `bson:"startSnippet" json:"startSnippet" validate:"required"`
-	MidSnippet   string `bson:"midSnippet,omitempty" json:"midSnippet,omitempty" validate:"required"`
+	MidSnippet   string `bson:"midSnippet,omitempty" json:"midSnippet,omitempty"`
 	EndSnippet   string `bson:"endSnippet" json:"endSnippet" validate:"required"`
 }
 
 type GenerateProblem struct {
 	ID           string        `bson:"_id,omitempty" json:"id,omitempty"`
-	Title        string        `bson:"title" json:"title"`
-	Description  string        `bson:"description" json:"description"`
-	Difficulty   string        `bson:"difficulty" json:"difficulty"`
-	TestCases    []TestCase    `bson:"testCases" json:"testCases"`
-	CodeSnippets []CodeSnippet `bson:"codeSnippets" json:"codeSnippets"`
+	Title        string        `bson:"title" json:"title" validate:"required"`
+	Description  string        `bson:"description" json:"description" validate:"required"`
+	Difficulty   string        `bson:"difficulty" json:"difficulty" validate:"required"`
+	TestCases    []TestCase    `bson:"testCases" json:"testCases" validate:"required,dive"`
+	CodeSnippets []CodeSnippet `bson:"codeSnippets" json:"codeSnippets" validate:"required,dive"`
 	Editorial    string        `bson:"editorial" json:"editorial,omitempty"`
 	Topic        string        `bson:"topic" json:"topic,omitempty"`
 }
--- middleware/request.validators.go
+++ middleware/request.validators.go
@@ -1,7 +1,7 @@
 package middleware
 
 import (
-	"coderX/models"
+	dto "coderX/DTO"
 	"coderX/utils/fomatter"
 	"coderX/utils/json"
 	"coderX/utils/validators"
@@ -18,7 +18,7 @@ func ProblemCreation(next http.Handler) http.Handler{
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 
-			var payload models.Problem
+			var payload dto.GenerateProblem
 			
 			err := json.DecodeFromJSON(r,&payload)
 			if err != nil{

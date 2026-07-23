57b42c29be7b8eba956c90f542bd551bb9025c88
Abhinav <AbhinavSunil@hotmail.com>
2026-07-23 11:34:58 +0530

    Feat : Sucessfully added the update CRUD

--- controllers/problem.controller.go
+++ controllers/problem.controller.go
@@ -92,3 +92,52 @@ func (controller *ProblemController) GetAllProblems(w http.ResponseWriter, r *ht
 
 	fomatter.SucessResponse(w,http.StatusAccepted,"Problems retrieved succesfully",response)
 }
+
+func (controller *ProblemController) UpdateProblem(w http.ResponseWriter, r *http.Request){
+
+	problemID := chi.URLParam(r,"id")
+	if problemID == ""{
+		fomatter.ErrorResponse(w,http.StatusBadRequest,"ProblemID is required",nil)
+		return
+	}
+
+	dtoPayload := r.Context().Value(middleware.PayloadContextKey).(dto.GenerateProblem)
+	
+	var testCases []models.TestCase
+	for _, tc := range dtoPayload.TestCases {
+		testCases = append(testCases, models.TestCase{
+			Input:  tc.Input,
+			Output: tc.Output,
+		})
+	}
+
+	var codeSnippets []models.CodeSnippet
+	for _, cs := range dtoPayload.CodeSnippets {
+		codeSnippets = append(codeSnippets, models.CodeSnippet{
+			Language:     cs.Language,
+			StartSnippet: cs.StartSnippet,
+			MidSnippet:   cs.MidSnippet,
+			EndSnippet:   cs.EndSnippet,
+		})
+	}
+
+	// Create your final Model representation (Controller Layer)
+	modelPayload := models.Problem{
+		Title:        dtoPayload.Title,
+		Description:  dtoPayload.Description,
+		Difficulty:   dtoPayload.Difficulty,
+		TestCases:    testCases, 
+		CodeSnippets: codeSnippets, 
+		Editorial:    dtoPayload.Editorial,
+		Topic:        dtoPayload.Topic,
+	}
+
+	response, err := controller.service.UpdateProbelm(r.Context(), problemID,&modelPayload)
+
+	if err != nil {
+		fomatter.ErrorResponse(w, http.StatusInternalServerError, "Failed to update problem", err)
+		return
+	}
+
+	fomatter.SucessResponse(w, http.StatusOK, "Problem updated successfully", response)
+}
--- routers/problem.router.go
+++ routers/problem.router.go
@@ -12,4 +12,5 @@ func ProblemRoutes(r chi.Router, pc *controllers.ProblemController){
 	r.With(middleware.ProblemCreation).Post("/create-problem",pc.CreateProblem)
 	r.Get("/{id}",pc.GetProblem)
 	r.Get("/",pc.GetAllProblems)
+	r.With(middleware.ProblemCreation).Put("/{id}",pc.UpdateProblem)
 }
